a8668506415839e741c19322c2a9fae4e9a94c03
Coinbra1999 <guilherme.brito@sptech.school>
2022-11-29 11:15:13 -0300

    wordcloud

--- guilherme2.py
+++ guilherme2.py
@@ -6,7 +6,7 @@ from PIL import Image
 import mysql.connector
 import psutil
 
-bdsql = mysql.connector.connect(host="localhost", user="root", password="sptech", database="airData", autocommit=True)
+bdsql = mysql.connector.connect(host="localhost", user="aluno", password="sptech", database="airData", autocommit=True)
 
 mycursor = bdsql.cursor()
 
@@ -16,17 +16,19 @@ mycursor.execute(querry)
 
 resposta = mycursor.fetchall()
 
-ocorrencias = []
+processos = []
 for row in resposta:
-    ocorrencias.append([str(row[0])] * int(row[1]))
+    for row2 in range(0, int(row[1])):
+        processos.append(str(row[0]))
+
 
 # ocorrencias = ["dashboard_não_aparece"] * 60 + ["dashboard_travado "] * 160 + ["temperatura_elevada"] * 120 + ["cpu_consumo_elevado"] * 80 + ["aplicação_lenta"] * 100 + ["upload_lento"] * 60 + ["download_lento"] * 20 + ["memoria_cheia"] * 20 + ["alerta_dispara"] * 20 + ["dados_incorretos"] * 20 + ["disco_consumo_elevado"] * 20 + ["usuario_não_pode_ser_cadastrado"] * 40 + ["maquina_não_pode_ser_cadastrada"] * 20 + ["login_não_funciona"] * 20
 
-print(ocorrencias)
+# print(processos)
 
 # amostra = sample(ocorrencias, 300)
 
-texto = " ".join(ocorrencias)
+texto = " ".join(processos)
 
 # mask = np.array(Image.open("cloud.png"))
 # word_cloud = WordCloud(collocations = False, background_color = 'black', mask=mask, contour_color='white', contour_width=3).generate(texto)
@@ -36,8 +38,8 @@ texto = " ".join(ocorrencias)
 # plt.axis("off")
 # plt.show()
 
-word_cloud = WordCloud(collocations = False, background_color = 'black').generate(texto)
+word_cloud = WordCloud(collocations = False, background_color = 'white').generate(texto)
 
-plt.imshow(word_cloud, interpolation='bilinear')
+plt.imshow(word_cloud)
 plt.axis("off")
 plt.show()
--- site_institucional/src/database/script-tabelas.sql
+++ site_institucional/src/database/script-tabelas.sql
@@ -240,23 +240,24 @@ Insert INTO servidor values(1,1);
 # antes de inserir esses dados abaixo, 
 # cadastre o servidor na API python e 
 ## mude o a variável @macAddress para o seu endereço mac!!!!
-SET @macAddress = 'b0:68:e6:f5:21:e1';
+SET @macAddress = 'b0:68:e6:f5:21:7f';
 
 INSERT INTO componente (idComponente, fkServidor, tipoComponente, nomeComponente, memoria, tipoMemoria) VALUES 
-(null, 'b0:68:e6:f5:21:e1', 'CPU', 'CPU1', 4.00, 'Registrador');
+(null, 'b0:68:e6:f5:21:7f', 'CPU', 'CPU1', 4.00, 'Registrador');
 INSERT INTO componente (idComponente, fkServidor, tipoComponente, nomeComponente, memoria, tipoMemoria) VALUES 
-(null, 'b0:68:e6:f5:21:e1', 'RAM', 'RAM1', 16.00, 'RAM');
+(null, 'b0:68:e6:f5:21:7f', 'RAM', 'RAM1', 16.00, 'RAM');
 INSERT INTO componente (idComponente, fkServidor, tipoComponente, nomeComponente, memoria, tipoMemoria) VALUES 
-(null, 'b0:68:e6:f5:21:e1', 'DISK', 'DISK1', 500.00, 'HD');
+(null, 'b0:68:e6:f5:21:7f', 'DISK', 'DISK1', 500.00, 'HD');
 
+select * from componente;
 INSERT INTO parametro (fkMetrica, fkComponente_idComponente, fkComponente_fkServidor) VALUES 
-(1, 4, 'b0:68:e6:f5:21:e1');
+(1, 1, 'b0:68:e6:f5:21:7f');
 INSERT INTO parametro (fkMetrica, fkComponente_idComponente, fkComponente_fkServidor) VALUES 
 (4, 1, @macAddress);
 INSERT INTO parametro (fkMetrica, fkComponente_idComponente, fkComponente_fkServidor) VALUES 
-(2, 5, @macAddress);
+(2, 2, @macAddress);
 INSERT INTO parametro (fkMetrica, fkComponente_idComponente, fkComponente_fkServidor) VALUES 
-(3, 6, @macAddress);
+(3, 3, @macAddress);
 
 SELECT * FROM usuario;
 SELECT * FROM empresa;
